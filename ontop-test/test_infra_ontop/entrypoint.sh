42eecd9a541d252c6d4c6e9ac9c4b20b050c78b5
Mariana Meireles <marian.meireles@gmail.com>
2024-11-26 16:30:28 +0100

    testing different curls

--- ontop-test/test_infra_ontop/entrypoint.sh
+++ ontop-test/test_infra_ontop/entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-set -e
+# set -e
 
-echo "Waiting for OMERO service to be available..."
+# echo "Waiting for OMERO service to be available..."
 
 # Loop until the OMERO service is reachable
-until curl -f http://localhost:4064; do
-    echo "OMERO service is not ready. Retrying in 5 seconds..."
-    sleep 5
-done
+# until curl -f http://localhost:4064; do
+#     echo "OMERO service is not ready. Retrying in 5 seconds..."
+#     sleep 5
+# done
 
-echo "OMERO service is available. Running scripts..."
+# echo "OMERO service is available. Running scripts..."
+
+curl -v http://localhost:4064
+curl -v http://localhost:8080
+
+curl -v http://omero:4064
+curl -v http://ontop:8080
+
 
 # Run the scripts
 /opt/project/insert_data.sh && python /opt/project/test_endpoint.py

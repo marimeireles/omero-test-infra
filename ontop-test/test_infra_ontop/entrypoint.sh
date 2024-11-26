1616f9c83226059b6244f3443d428913ef1833b6
Mariana Meireles <marian.meireles@gmail.com>
2024-11-26 16:13:17 +0100

    Change URL name from omero:#### to localhost:####

--- ontop-test/test_infra_ontop/entrypoint.sh
+++ ontop-test/test_infra_ontop/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "Waiting for OMERO service to be available..."
 
 # Loop until the OMERO service is reachable
-until curl -f http://omero:4064; do
+until curl -f http://localhost:4064; do
     echo "OMERO service is not ready. Retrying in 5 seconds..."
     sleep 5
 done
